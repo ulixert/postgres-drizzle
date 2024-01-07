f50316f3fe35eaf2c79fc6d8b09a17a6137ba569
matt <143686761+ulixert@users.noreply.github.com>
2024-01-07 02:00:44 -0800

    feat: add inserts

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,13 @@ import { and, eq, gt, gte, like, or } from 'drizzle-orm';
 import express, { Express } from 'express';
 
 import { db } from '@/db/index.js';
-import { profiles, users } from '@/db/schema.js';
+import {
+  categories,
+  posts,
+  postsToCategories,
+  profiles,
+  users,
+} from '@/db/schema.js';
 import { insertUserSchema } from '@/types/schemas.js';
 
 export const app: Express = express();
@@ -134,9 +140,9 @@ app.get('/api/v1/users/:id/posts', async (req, res) => {
   });
 });
 
-app.get('/api/v1/users/:userId/posts/categories', async (req, res) => {
+app.get('/api/v1/users/:userId/posts/categories', async (_, res) => {
   try {
-    const id = Number(req.params.userId);
+    // const id = Number(req.params.userId);
     // const result = await db.query.users.findFirst({
     //   where: eq(users.id, id),
     //   with: {
@@ -184,5 +190,131 @@ app.get('/api/v1/users/:userId/posts/categories', async (req, res) => {
       status: 'fail',
       message: 'User not found',
     });
+  }
+});
+
+// One to one
+app.post('/api/v1/users/new/signup', async (_, res) => {
+  const newUsers = await db
+    .insert(users)
+    .values({
+      fullName: 'Usman',
+      address: 'Lahore',
+      phone: '03000000000',
+      score: 100,
+    })
+    .returning({ userId: users.id });
+
+  await db.insert(profiles).values({
+    userId: newUsers[0].userId,
+    bio: 'This is my bio',
+  });
+
+  const result = await db.query.users.findFirst({
+    where: eq(users.id, newUsers[0].userId),
+    with: {
+      profile: true,
+    },
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: result,
+    },
+  });
+});
+
+// One to many
+app.post('/api/v1/users/new/post', async (_, res) => {
+  try {
+    ['post1', 'post2', 'post3'].forEach(async (post) => {
+      await db.insert(posts).values({
+        authorId: 1,
+        text: post,
+      });
+    });
+
+    const user = await db.query.users.findFirst({
+      where: eq(users.id, 1),
+      with: {
+        posts: true,
+      },
+    });
+
+    res.status(201).json({
+      status: 'success',
+      message: 'Posts created',
+      data: { user },
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(400).json({
+        status: 'fail',
+        message: err.message,
+      });
+    }
+  }
+});
+
+// Many to many
+app.post('/api/v1/users/new/post/categories', async (_, res) => {
+  try {
+    const newCategories = await db
+      .insert(categories)
+      .values([
+        { name: 'category1' },
+        { name: 'category2' },
+        { name: 'category3' },
+      ])
+      .returning({ categoryId: categories.id });
+
+    const newPosts = await db
+      .insert(posts)
+      .values([
+        { authorId: 2, text: 'post1' },
+        { authorId: 2, text: 'post2' },
+        { authorId: 2, text: 'post3' },
+      ])
+      .returning({ postId: posts.id });
+
+    await db.insert(postsToCategories).values([
+      { postId: newPosts[0].postId, categoriesId: newCategories[0].categoryId },
+      { postId: newPosts[1].postId, categoriesId: newCategories[1].categoryId },
+      { postId: newPosts[2].postId, categoriesId: newCategories[2].categoryId },
+    ]);
+
+    const result = await db.query.users.findFirst({
+      where: eq(users.id, 2),
+      with: {
+        posts: {
+          with: {
+            postsToCategories: {
+              columns: {},
+              with: {
+                category: {
+                  columns: {
+                    name: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+
+    res.status(201).json({
+      status: 'success',
+      message: 'Posts created',
+      data: { result },
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(400).json({
+        status: 'fail',
+        message: err.message,
+      });
+    }
   }
 });
